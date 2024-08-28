87fa05ce25816d77a2b5536359016828885fc6ee
YurakunD3suyo <yurakund3sukedo0214@gmail.com>
2024-08-28 14:52:02 +0900

    Update: 自動更新コマンドの実装

--- cogs/update.py
+++ cogs/update.py
@@ -14,34 +14,23 @@ class Update(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    # 管理用サーバーにコマンドを追加 ------------------------
-    #サポートサーバーが利用可能かの認識用
-    support_enable = False
-
-    try:
-        #.envを読み込み
-        dotenv.load_dotenv()
-        #サポート鯖のIDを取得
-        support_id = os.getenv("OP_SERVER")
-        #strである場合はintに変換
-        if type(support_id) == str:
-            support_id = int(support_id)
-            support_obj = Object(support_id)
-
-            logging.debug(f"update -> サポートサーバーID: {support_id}")
-            support_enable = True
-
-        elif type(support_id) != None:
-            logging.exception("update -> サポートサーバーのID記述が不適切")
-        else:
-            logging.warn("update -> サポートサーバー未登録(管理コマンドが利用不可)")
-    except:
-        logging.exception("update -> サポートサーバーの読み込みに失敗")
-
     # 管理コマンド関係 -----------------------------
-    @app_commands.command(name="op-update", description="(管理コマンド)botをアップデートして自動再起動するのだ")
-    @app_commands.guilds(support_obj)
+    op = app_commands.Group(name="op", description="管理コマンドなのだ")
+
+    @op.command(name="update", description="(管理コマンド)botをアップデートして自動再起動するのだ") 
     async def op_update(self, interact: Interaction):
+        #Botの所有者かどうかの判断
+        appinfo = await self.bot.application_info()
+        bot_owner = appinfo.owner.id
+
+        #管理者でない場合はメッセージを流して終わり
+        if bot_owner != interact.user.id:
+            await interact.response.send_message("このコマンドは実行できません。")
+            return
+
+        # Contextを取得
+        ctx = await commands.Context.from_interaction(interact)
+        
         # Embedを先に送信し、アップデートを促す
         #embedを作成
         embed = Embed(
@@ -53,27 +42,44 @@ class Update(commands.Cog):
             name="アップデート状態",
             value="`githubから取得中...`"
         )
-        interact.response.send_message(embed=embed)
+        message = await ctx.send(embed=embed)
 
         #githubからpullする
         result = subprocess.run(['git', 'pull'], capture_output=True, text=True)
         output = result.stdout
         error = result.stderr
 
+        logging.debug(f"update -> Bot Updated (output: {output}, err: {error})")
+
         #エラーがない場合は続行、エラーの場合は停止
         if len(error) <= 0:
+            # すでに最新の場合は起動したままにする。
+            if output == "Already up to date.\n":
+                embed = Embed(
+                    title="すでに最新なのだ！",
+                    description="更新の必要はありません。",
+                    color=Color.green()
+                )
+                embed.add_field(
+                    name="アップデート状態",
+                    value=f"`{output}`"
+                )
+                await message.edit(embed=embed)
+                return
+            
             embed = Embed(
-                title="更新中なのだ...",
-                description="しばらくお待ちください...",
+                title="更新中できたのだ！！",
+                description="アップデートが完了しました！",
                 color=Color.green()
             )
             embed.add_field(
                 name="アップデート状態",
-                value="`アップデート完了、再起動します`"
+                value=f"`{output}`"
             )
-            interact.response.edit_message(embed=embed)
-            asyncio.sleep(2)
+            await message.edit(embed=embed)
+            await asyncio.sleep(2)
             sys.exit()
+
         else: 
             embed = Embed(
                 title="更新に失敗したのだ",
@@ -84,7 +90,7 @@ class Update(commands.Cog):
                 name="アップデート状態",
                 value=f"`{error}`"
             )
-            interact.response.edit_message(embed=embed)
+            await message.edit(embed=embed)
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(Update(bot))
