2cc1b3cdc96c8aa6e168183e55c0740320e67939
YurakunD3suyo <yurakund3sukedo0214@gmail.com>
2024-09-25 01:03:08 +0900

    Fix: cogの修正

--- cogs/utils/translate.py
+++ cogs/utils/translate.py
@@ -5,31 +5,39 @@ from discord.ext import commands
 from discord import app_commands, Interaction, Embed, Color
 from discord.app_commands import Choice
 
-@app_commands.command(name="translate", description="翻訳コマンドなのだ")
-@app_commands.choices(lang=[
-    Choice(name="日本語", value="JA"),
-    Choice(name="英語", value="EN"),
-])
-async def translate(self, interact: Interaction, sentence, target_lang: str):
-    auth = os.getenv("DEEPL_AUTH_KEY")
-    translator = deepl.Translator(auth)
+class Translate( commands.Cog ):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
 
-    result = translator.translate_text(sentence, target_lang=target_lang)
+    @app_commands.command(name="translate", description="翻訳コマンドなのだ")
+    @app_commands.choices(lang=[
+        Choice(name="日本語", value="JA"),
+        Choice(name="英語", value="EN"),
+    ])
+    async def translate(self, interact: Interaction, sentence, target_lang: str):
+        auth = os.getenv("DEEPL_AUTH_KEY")
+        translator = deepl.Translator(auth)
 
-    embed = Embed(
-        title="翻訳したのだ！",
-        description="正常に翻訳しました。結果は以下の通りです。",
-        color=Color.green()
-    )
-    embed.add_field(
-        name="翻訳前",
-        value=sentence
-    )
-    embed.add_field(
-        name="翻訳後",
-        value=result
-    )
+        result = translator.translate_text(sentence, target_lang=target_lang)
 
-    await interact.response.send_message(embed=embed)
+        embed = Embed(
+            title="翻訳したのだ！",
+            description="正常に翻訳しました。結果は以下の通りです。",
+            color=Color.green()
+        )
+        embed.add_field(
+            name="翻訳前",
+            value=sentence
+        )
+        embed.add_field(
+            name="翻訳後",
+            value=result
+        )
 
-    translator.close()
+        await interact.response.send_message(embed=embed)
+
+        translator.close()
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(Translate(bot))
+
