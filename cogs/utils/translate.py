2fdc1beb93c332d1ff3d6e6c8d56109d2d4f4dde
YurakunD3suyo <yurakund3sukedo0214@gmail.com>
2024-09-25 00:56:54 +0900

    Update; 翻訳機能の追加

---
+++ cogs/utils/translate.py
@@ -0,0 +1,35 @@
+import deepl
+import os
+
+from discord.ext import commands
+from discord import app_commands, Interaction, Embed, Color
+from discord.app_commands import Choice
+
+@app_commands.command(name="translate", description="翻訳コマンドなのだ")
+@app_commands.choices(lang=[
+    Choice(name="日本語", value="JA"),
+    Choice(name="英語", value="EN"),
+])
+async def translate(self, interact: Interaction, sentence, target_lang: str):
+    auth = os.getenv("DEEPL_AUTH_KEY")
+    translator = deepl.Translator(auth)
+
+    result = translator.translate_text(sentence, target_lang=target_lang)
+
+    embed = Embed(
+        title="翻訳したのだ！",
+        description="正常に翻訳しました。結果は以下の通りです。",
+        color=Color.green()
+    )
+    embed.add_field(
+        name="翻訳前",
+        value=sentence
+    )
+    embed.add_field(
+        name="翻訳後",
+        value=result
+    )
+
+    await interact.response.send_message(embed=embed)
+
+    translator.close()
